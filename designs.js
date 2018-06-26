aafed59d40200ecd0e0b9a49afbaeb77d923d884
Tonerolima <tonero91@gmail.com>
2018-06-26 20:21:21 +0100

    code refactor to improve readability and use of ES6 keywords let and const

--- designs.js
+++ designs.js
@@ -1,30 +1,42 @@
 // Select color input
-let $colorInput = $('#colorPicker');
+const $colorInput = $('#colorPicker');
+const $canvas = $('#pixelCanvas');
 
-// When size is submitted by the user, call makeGrid() and prevent default buton behavior
+/* 
+** When size is submitted by the user, call makeGrid()
+** and prevent default buton behavior
+*/
 $('form').submit(function(event){
-	makeGrid();
 	event.preventDefault();
+	/*
+	** Get the values from the form.
+	** parseInt converts the values from text to integer
+	*/
+	let $height =  parseInt($('#inputHeight').val());
+	let $width =  parseInt($('#inputWeight').val());
+
+	//call the makeGrid function with the input parameters
+	makeGrid($height, $width);
 })
+
 
 // When a cell is clicked, change the color to the colorPicker value
 $('#pixelCanvas').on('click', 'td', function(event){
 	$(this).css('background-color', $colorInput.val());
-});
+})
 
-function makeGrid(){
-	// Get the values from the form
-	let $h =  parseInt($('#inputHeight').val());
-	let $w =  parseInt($('#inputWeight').val());
 
-	// Remove all table rows
-	$('#pixelCanvas').find('tr').remove();
+function makeGrid(height, width){
+	// Remove all rows from the table
+	$canvas.find('tr').remove();
 
-	//pre-process the number of columns and wrap them in a row
-	let rows = "<tr>" + "<td></td>".repeat($w) + "</tr>";
+	/* pre-process the number of columns and wrap them in a row
+	** this eliminates the use of two loops to create the grid
+	*/
+	let rows = "<tr>" + "<td></td>".repeat(width) + "</tr>";
 
-	//append rows to table "height" number of times
-	for(var y = 1; y <= $h; y++) {
-		$('#pixelCanvas').append(rows);
+	//append rows to table, "height" number of times
+	for(let y = 1; y <= height; y++) {
+		$canvas.append(rows);
 	}
 }
