424c85008c2c21d6ebbaeb747239c35a4c012bc6
Tonerolima <tonero91@gmail.com>
2018-06-27 09:57:14 +0100

    introduce use of tbody to improve performance, remove redundant method parseInt()

--- designs.js
+++ designs.js
@@ -2,18 +2,14 @@
 const $colorInput = $('#colorPicker');
 const $canvas = $('#pixelCanvas');
 
-/* 
-** When size is submitted by the user, call makeGrid()
-** and prevent default buton behavior
-*/
+//When size is submitted by the user, call makeGrid()
 $('form').submit(function(event){
+	//prevent default buton behavior
 	event.preventDefault();
-	/*
-	** Get the values from the form.
-	** parseInt converts the values from text to integer
-	*/
-	let $height =  parseInt($('#inputHeight').val());
-	let $width =  parseInt($('#inputWeight').val());
+
+	//Get the values from the form.
+	let $height =  $('#inputHeight').val();
+	let $width =  $('#inputWeight').val();
 
 	//call the makeGrid function with the input parameters
 	makeGrid($height, $width);
@@ -27,16 +23,22 @@ $('#pixelCanvas').on('click', 'td', function(event){
 
 
 function makeGrid(height, width){
-	// Remove all rows from the table
-	$canvas.find('tr').remove();
+	// Remove existing grid, if any
+	$('tbody').remove();
+
+	/* create a new tbody element that will hold the rows
+	** this improves overall performance as we only have to remove one
+	** element when clearing the grid (especially a large grid)
+	*/
+	$canvas.append("<tbody></tbody>");
 
 	/* pre-process the number of columns and wrap them in a row
 	** this eliminates the use of two loops to create the grid
 	*/
 	let rows = "<tr>" + "<td></td>".repeat(width) + "</tr>";
 
-	//append rows to table, "height" number of times
+	//append rows to tbody, "height" number of times
 	for(let y = 1; y <= height; y++) {
-		$canvas.append(rows);
+		$('tbody').append(rows);
 	}
 }
